9f362d2ab5cacd4c0784b31557135b506a68acdd
xingkuan <xingkuan.li@gmail.com>
2020-07-28 14:40:15 -0700

    init save

---
+++ pyDI/pyDI/oraJobGlobalVars.py
@@ -0,0 +1,9 @@
+#
+
+dbs = { "RMAN": {"url": "crmrman01:1521/RMAN01", "user": "vertsnap", "pwd": "xxx"},
+       "JOTPP": {"url": "xxx:1523/xxx", "user": "sewn", "pwd": "xxxx"}, 
+       "JOTPPGU": {"url": "xxx:1523/xxx", "user": "guess", "pwd": "xxxx"}, 
+       "DPRD": {"url": "rxx:1523/xxx", "user": "system", "pwd": "xxxx"}, 
+        "XX": "test2"
+        }
+#print(dbs.get("JOTPP"))
